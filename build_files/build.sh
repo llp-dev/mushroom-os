4c74ffb7e92eee272b60188f66cfc25ba530ae6b
Leonardo Lopes Pereira <leonardolopespereira@outlook.com>
2026-03-07 15:52:04 +0100

    Enhance build script with dev tools

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,9 +2,28 @@
 
 set -ouex pipefail
 
-dnf5 install -y emacs \
-                firefox \
-                tmux \
-                zsh
+### 1 Password
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+dnf5 config-manager addrepo --from-repofile=https://downloads.1password.com/linux/rpm/stable/1password.repo
+dnf5 install -y 1password
+
+### C
+dnf5 install -y clang \
+                clang-tools-extra \
+                make
+
+### Emacs
+dnf5 install -y emacs-pgtk
+
+### Go
+dnf5 install -y golang \
+                gopls
+
+### Rust
+dnf5 install -y cargo \
+                rustc
+
+### Rust
+dnf5 install -y zsh
 
 systemctl enable podman.socket
