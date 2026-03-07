26ad159bc3668052af197f3f8588ef1ee1db6d7e
Leonardo Lopes Pereira <leonardolopespereira@outlook.com>
2026-03-07 15:56:07 +0100

    Change 1Password installation to use RPM package
    Updated the installation method for 1Password to use the latest RPM package.

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,9 +3,9 @@
 set -ouex pipefail
 
 ### 1 Password
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-dnf5 config-manager addrepo --from-repofile=https://downloads.1password.com/linux/rpm/stable/1password.repo
-dnf5 install -y 1password
+curl -LO https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
+dnf5 install -y ./1password-latest.rpm
+rm 1password-latest.rpm
 
 ### C
 dnf5 install -y clang \
