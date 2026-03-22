2c13c4fc2c536d288f4aaf4055bf2f92719ded11
Leonardo Lopes Pereira <leonardolopespereira@outlook.com>
2026-03-22 02:31:22 +0100

    Refactor build script for improved clarity and organization
    Updated package installation script to improve readability and organization. Added comments for better clarity on repository sources and grouped related installations.

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,72 +1,80 @@
 #!/bin/bash
 
-set -ouex pipefail
+set -eux
 
-### 1 Password
+# 1Password
 cat <<'EOF' > /etc/yum.repos.d/1password.repo
 [1password]
 name=1Password Stable Channel
-baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
+baseurl=https://downloads.1password.com/linux/rpm/stable/$basearch
 enabled=1
 repo_gpgcheck=1
 gpgcheck=1
 gpgkey=https://downloads.1password.com/linux/keys/1password.asc
 EOF
-dnf5 install -y 1password
 
-### C
-dnf5 install -y clang \
-                clang-tools-extra \
-                glslang \
-                make
-
-### Emacs
-dnf5 install -y emacs-pgtk
-
-### Go
-dnf5 install -y golang \
-                gopls
-
-### Java
+# Adoptium (Java)
 cat <<'EOF' > /etc/yum.repos.d/adoptium.repo
 [Adoptium]
 name=Adoptium
-baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/\$releasever/\$basearch
+baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/$releasever/$basearch
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
 EOF
-dnf5 install -y temurin-25-jdk
 
-### Python
-dnf5 install -y black \
+# HashiCorp (Terraform)
+dnf5 config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+
+# Lazygit (COPR)
+dnf5 copr enable -y dejan/lazygit
+
+dnf5 install -y 1password \
+                7zip \
+                ansible \
+                bat \
+                black \
+                btop \
+                cargo \
+                clang \
+                clang-tools-extra \
+                direnv \
+                fd-find \
+                fzf \
+                gcc \
+                git \
+                golang \
+                gopls \
+                glslang \
+                helm \
+                htop \
+                jq \
+                k9s \
+                kubectl \
+                lazygit \
+                markdown \
+                make \
+                neovim \
+                pandoc \
+                podman-compose \
+                podman-docker \
                 pipx \
                 poetry \
                 python3-isort \
                 python3-pip \
                 python3-pyflakes \
-                python3-pytest
-                
-### Rust
-dnf5 install -y cargo \
-                rust-analyzer \
-                rustc
-
-### Shell
-dnf5 install -y ShellCheck \
-                shfmt
-
-### Tools
-dnf5 config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-
-dnf5 install -y ansible \
-                direnv \
-                fd-find \
-                kubectl \
-                markdown \
-                pandoc \
+                python3-pytest \
                 ripgrep \
+                rust-analyzer \
+                rustc \
+                screen \
+                ShellCheck \
+                shfmt \
+                temurin-25-jdk \
                 terraform \
+                unzip \
+                wl-clipboard \
+                zip \
                 zsh
 
 ### Cleanup
